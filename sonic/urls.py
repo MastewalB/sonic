cdbedcdb75f7671d7ff69cf08c2fef6bed517145
Nah7wzy <tamnahom7@gmail.com>
2023-02-03 01:00:42 +0300

    urls added

--- sonic/urls.py
+++ sonic/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ..users import SignupView, LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
+
