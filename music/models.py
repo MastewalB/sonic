cc0f17852059d9f8f9a013c59ca320dd93fb6bfd
Nah7wzy <tamnahom7@gmail.com>
2023-03-08 22:21:37 +0300

    music model first draft

---
+++ music/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Artist(models.Model):
+	name = models.CharField(max_length="55")
+	picture = models.URLField(blank=True)
+	
+class Album(models.Model):
+	artist = models.ForeignKey(Artist, on_delete = models.CASCADE)
+	name = models.CharField(max_length=150, blank=False, null=False)
+	cover = models.URLField(blank=True)
+
+class Song(models.Model):
+	title = models.CharField(max_length="25")
+	s_artist = models.ForeignKey(Artist, on_delete = models.CASCADE)
+	s_album = models.ForeignKey(Album, on_delete = models.CASCADE)
+
