b9e3baf3f6a83a51ea10631d6a022802ffb740c8
MastewalB <se.mastewal.belete@gmail.com>
2023-03-18 22:40:50 +0300

    fixed todo - userid

--- users/models.py
+++ users/models.py
@@ -36,7 +36,6 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser):
-    # TODO - userId field
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=20)
