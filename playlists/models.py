9d5a5c4f2401e7f9394296781e9803dbef668ac6
AtoBrightSide <meseleabel99@gmail.com>
2023-03-19 16:04:28 +0300

    removed podcast item foreign key for now

--- playlists/models.py
+++ playlists/models.py
@@ -5,6 +5,5 @@ from users.models import User
 class Playlist(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     song_item = models.ForeignKey(Song, on_delete=models.CASCADE, null=True)
-    podcast_item = models.ForeignKey(Podcast, on_delete=models.CASCADE, null=True)
 
     playlist_title = models.CharField(max_length=255)
