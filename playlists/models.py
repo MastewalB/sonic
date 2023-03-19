f80787131c761d02b2e4ae9b54f004e37b623782
AtoBrightSide <meseleabel99@gmail.com>
2023-03-19 16:54:15 +0300

    updated playlist design on models and view

--- playlists/models.py
+++ playlists/models.py
@@ -3,7 +3,10 @@ from users.models import User
 
 # Create your models here.
 class Playlist(models.Model):
+    playlist_id = models.BigAutoField(primary_key=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    song_item = models.ForeignKey(Song, on_delete=models.CASCADE, null=True)
-
     playlist_title = models.CharField(max_length=255)
+
+class PlaylistItems(models.Model):
+    playlist_id = models.BigAutoField()
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
