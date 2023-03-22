6a1aacffd1288d4bd49425fadc24b5fca5bcd2b6
AtoBrightSide <meseleabel99@gmail.com>
2023-03-22 15:38:57 +0300

    refractored name of serializer

--- playlists/views.py
+++ playlists/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
-from playlists.serializers import PlaylistSerialier, PlaylistItemsSerializer
+from playlists.serializers import PlaylistSerializer, PlaylistItemsSerializer
 from playlists.models import Playlist, PlaylistItems
 
 # Create your views here.
 class PlaylistView(APIView):
     def post(self, request):
-        serializer = PlaylistSerialier(data=request.data)
+        serializer = PlaylistSerializer(data=request.data)
         try:
             serializer.is_valid(raise_exception=True)
             serializer.save()
@@ -16,14 +16,11 @@ class PlaylistView(APIView):
         except:
             return Response(status=status.HTTP_400_BAD_REQUEST)
     
-    def get(self, playlist_id):
-        playlist = get_object_or_404(Playlist, pk=playlist_id)
-        serialize = PlaylistItemsSerializer(playlist)
+    def delete(self, request):
+        serializer = PlaylistSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-        return Response(serialize.data)
-    
-    def delete(self, playlist_id):
-        playlist = get_object_or_404(Playlist, pk=playlist_id)
+        playlist = get_object_or_404(Playlist, playlist_id=serializer.data.playlist_id)
         playlist.delete()
 
         return Response(status= status.HTTP_204_NO_CONTENT)
@@ -36,10 +33,17 @@ class PlaylistItemsView(APIView):
             serializer.save()
             return Response(serializer.data)
         except:
-            return Response(status=status.HTTP_400_BAD_REQUEST)        
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    def get(self, playlist_id, song_id):
-        playlist = get_object_or_404(PlaylistItems, pk=playlist_id)
-        serialize = PlaylistItemsSerializer(playlist)
+    def get(self, request):
+        serializer = PlaylistItemsSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-        return Response(serialize.data)
+    def delete(self, request):
+        serializer = PlaylistItemsSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        playlistItem = get_object_or_404(PlaylistItems, playlist_id=serializer.data.playlist_id, song_id=serializer.data.song_id)
+        playlistItem.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
