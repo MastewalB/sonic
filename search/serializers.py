2dbd61e4fb452414d8301174398bb65c4851f4ab
Nahom Fufa <nahomn7@gmail.com>
2023-03-26 20:49:26 +0300

    serializer fix

--- search/serializers.py
+++ search/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import Search
 
+
 class SearchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Search
-        fields = '__all__'
+        fields = []
--- search/views.py
+++ search/views.py
@@ -7,23 +7,26 @@ from .serializers import SearchSerializer
 from music.models import Album, Artist, Song
 from music.serializers import AlbumSerializer, ArtistSerializer, SongSerializer
 
+
 class SearchView(generics.ListAPIView):
     serializer_class = SearchSerializer
 
     def get_queryset(self):
-        query = self.request.query_params.get('q', None)
+        query = self.request.query_params.get('search_query', None)
+        print(query)
         if query is None:
             return Search.objects.none()
 
-        album_query = Q(title__icontains=query)
-        artist_query = Q(a_name__icontains=query)
-        song_query = Q(title__icontains=query) | Q(s_artist__a_name__icontains=query) | Q(s_album__title__icontains=query)
-        
+        album_query = Q(name__icontains=query)
+        artist_query = Q(name__icontains=query)
+        song_query = Q(title__icontains=query) | Q(
+            s_artist__name__icontains=query) | Q(s_album__name__icontains=query)
+
         search_results = [
             {
                 'type': 'album',
                 'data': AlbumSerializer(album).data,
-                'score': album.title.name().count(query.lower()),
+                'score': album.name.lower().count(query.lower()),
             }
             for album in Album.objects.filter(album_query)
         ] + [
@@ -41,12 +44,14 @@ class SearchView(generics.ListAPIView):
             }
             for song in Song.objects.filter(song_query)
         ]
-        
+
         # Sort the results by score in descending order
-        search_results = sorted(search_results, key=lambda x: x['score'], reverse=True)
+        search_results = sorted(
+            search_results, key=lambda x: x['score'], reverse=True)
 
         # Save the search query to the database
         search = Search(search_query=query, search_type='all')
         search.save()
+        print(search_results)
 
         return search_results
