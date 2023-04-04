562d430c527e3ef921a00378fbfa69cafc422470
Abel Mesele <se.abel.mesele@gmail.com>
2023-04-04 10:35:21 +0300

    removed duplicate model

--- sonic/settings.py
+++ sonic/settings.py
@@ -39,11 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playlists',
     'music',
     'users',
     'rest_framework',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
     'playlists',
 ]
 
