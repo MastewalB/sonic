8f77cc32b85be1353825ed8ed7044f7dada4c338
Abel Mesele <se.abel.mesele@gmail.com>
2023-04-05 22:39:08 +0300

    removed duplicate app in settings, and updated env

--- sonic/settings.py
+++ sonic/settings.py
@@ -27,8 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-
+SECRET_KEY = 'django-insecure-+m#2o!4q9ml%57x0n)50kndujf!ktgec=xdemz5%+u9!ypojiu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -45,11 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'playlists',
     'music',
     'users',
     'rest_framework',
-    'rest_framework_simplejwt',
-    'playlists',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
