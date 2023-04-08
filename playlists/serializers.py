a973689eb4a037b1c530a27111c1773323f6edfd
MastewalB <se.mastewal.belete@gmail.com>
2023-04-08 14:08:26 +0300

    Playlist end-points tested

--- playlists/serializers.py
+++ playlists/serializers.py
@@ -1,19 +1,22 @@
 from rest_framework import serializers
 from playlists.models import Playlist, PlaylistItems
 
+
 class CreatePlaylistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Playlist
         fields = ['created_by', 'playlist_title', 'id']
-    
+        read_only_fields = ['created_by', 'id']
+
     def create(self, validated_data):
         playlist = Playlist(
-            created_by=validated_data['created_by'],
+            created_by=self.context['created_by'],
             playlist_title=validated_data['playlist_title']
         )
 
         playlist.save()
         return playlist
+
 
 class PlaylistSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,7 +25,9 @@ class PlaylistSerializer(serializers.ModelSerializer):
 
         read_only_fields = ['created_by', 'id']
 
+
 class PlaylistItemsSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = PlaylistItems
         fields = ['playlist_id', 'song_id']
--- playlists/urls.py
+++ playlists/urls.py
@@ -3,7 +3,9 @@ from .views import PlaylistItemsView, PlaylistView, CreatePlaylistView, UserPlay
 
 urlpatterns = [
     path('create/', CreatePlaylistView.as_view()),
+    path('add/', PlaylistItemsView.as_view()),
+    path('remove/', PlaylistItemsView.as_view()),
+    path('delete/<str:playlist_id>/', PlaylistView.as_view()),
     path('<str:playlist_id>/', PlaylistView.as_view()),
     path('user_playlist/<str:user_id>/', UserPlaylistView.as_view()),
-    path('add_item/', PlaylistItemsView.as_view()),
 ]
