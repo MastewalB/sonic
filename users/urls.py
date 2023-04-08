dba3a3b6bdfb46c47a6224358c2a71074b8801e6
MastewalB <se.mastewal.belete@gmail.com>
2023-04-08 11:44:06 +0300

    Update Profile View fix

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
+from django.contrib import admin
 from django.urls import path
-from users.views import SignupView, LoginView, UpdateProfileView, activate
+from users.views import SignupView, LoginView, UpdateProfileView, activate, login
 
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='sign_up'),
     path('login/', LoginView.as_view(), name='login'),
     path('activate/<uidb64>/<token>', activate, name='activate'),
-    path('update/<str:id>', UpdateProfileView.as_view(), name='update')
+    path('update/', UpdateProfileView.as_view(), name='update')
 ]
