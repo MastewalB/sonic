02d12a0f08c9f3975ffdfd34238694c37aa82dfe
MastewalB <se.mastewal.belete@gmail.com>
2023-05-06 13:42:00 +0300

    Serializer Fix -PlaylistDetailSerializer

--- music/serializers.py
+++ music/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from music.models import Artist, Album, Song
 
+
 class ArtistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Artist
@@ -14,6 +15,16 @@ class AlbumSerializer(serializers.ModelSerializer):
 
 
 class SongSerializer(serializers.ModelSerializer):
+    song_file = serializers.SerializerMethodField()
+
     class Meta:
         model = Song
-        fields = ['id', 'title', 's_artist', 's_album', 'song_file', 'content_type']
+        fields = ['id', 'title', 's_artist',
+                  's_album', 'song_file', 'content_type']
+
+    def get_song_file(self, obj):
+        request = self.context.get('request')
+        file_url = obj.song_file.url
+        if request is not None:
+            return request.build_absolute_uri(file_url)
+        return file_url
--- playlists/serializers.py
+++ playlists/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from playlists.models import Playlist, PlaylistItems
+from music.models import Song
+from music.serializers import SongSerializer
+from users.serializers import UserPublicSerializer
 
 
 class CreatePlaylistSerializer(serializers.ModelSerializer):
@@ -31,3 +34,24 @@ class PlaylistItemsSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlaylistItems
         fields = ['playlist_id', 'song_id']
+
+
+class PlaylistDetailSerializer(serializers.ModelSerializer):
+    items = serializers.SerializerMethodField()
+    created_by = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Playlist
+        fields = ('id', 'playlist_title', 'created_by', 'items')
+        read_only_fields = ['created_by', 'id']
+        depth = 2
+
+    def get_items(self, obj):
+        items = PlaylistItems.objects.filter(playlist_id=obj.id)
+        songs = []
+        for item in items:
+            songs.append(item.song_id)
+        return SongSerializer(songs, many=True, context=self.context).data
+
+    def get_created_by(self, obj):
+        return UserPublicSerializer(obj.created_by, context=self.context).data
