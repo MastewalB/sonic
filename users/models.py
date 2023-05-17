da64044c5e44bfd716c6ec8d2584c1b13a033dc1
MastewalB <se.mastewal.belete@gmail.com>
2023-05-17 06:01:48 +0300

    Custom create_superuser command

--- users/models.py
+++ users/models.py
@@ -23,9 +23,11 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(username, password, **extra_fields)
 
-    def create_superuser(self, username, password=None, **extra_fields):
+    def create_superuser(self, username, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('email', email)
 
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True')
@@ -45,6 +47,7 @@ class User(AbstractBaseUser):
     country = models.CharField(max_length=30)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'id'
     EMAIL_FIELD = 'email'
@@ -65,6 +68,8 @@ class User(AbstractBaseUser):
         return y
 
     def __str__(self):
+        if first_name == '':
+            return self.username
         return self.get_full_name()
 
     def has_perm(self, perm, obj=None):
