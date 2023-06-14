359c279803ba359c6d6a49a173b75f7d5bc38d00
Nahom Fufa <nahomn7@gmail.com>
2023-06-14 22:13:48 +0300

    inactive login fix

--- recommender/urls.py
+++ recommender/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import SongRecommender
 
 urlpatterns = [
-    path('recommend/<str:song_id>', SongRecommender.as_view(), name='recommend'),
+    path('recommend/<str:song_id>/', SongRecommender.as_view(), name='recommend'),
 ]
